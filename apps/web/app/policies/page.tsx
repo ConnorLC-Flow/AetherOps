ec3260698d55c8fdddaab85fc833da9a13505de5
AetherOps Deploy <aetherops@team.dev>
2026-05-27 08:12:30 +0000

    Fix DialogTrigger asChild type error

--- apps/web/app/policies/page.tsx
+++ apps/web/app/policies/page.tsx
@@ -64,7 +64,7 @@ export default function PoliciesPage() {
         </div>
         
         <Dialog open={isCreateModalOpen} onOpenChange={setIsCreateModalOpen}>
-          <DialogTrigger asChild>
+          <DialogTrigger>
             <Button>
               <Plus className="mr-2 h-4 w-4" />
               Create Policy
